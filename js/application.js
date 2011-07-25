2b78fed3d66c57e2b04f8b706dded914545df0fd
Chris Ricca <christopher.ricca@gmail.com>
2011-07-25 10:11:24 -0400

    Basic adding of work chunks working

--- js/application.js
+++ js/application.js
@@ -1,52 +1,114 @@
+// Use mustache-style templating
+_.templateSettings = {
+  interpolate : /\{\{(.+?)\}\}/g
+};
 
-
-// var WorkChunk = Backbone.Model.extend({
-//     // validate: function (attrs) {
-//     //     if (!attrs.to_do || !_.isString(attrs.to_do) || attrs.to_do.length === 0) {
-//     //         return "to_do must be a string with a length";
-//     //     }
-//     // }
-// });
-
-
-// var WorkSession = Backbone.Collection.extend({
-//     model: WorkChunk,
-//     
-//     initialize: function() {
-//         // do something?
-//     }
-// });
-
-// var work_session = new WorkSession;
 $(function(){
     var WorkChunk = Backbone.Model.extend({
-        
+        defaults: {
+            finished: false,
+            started_at: null,
+            display_time: '00:00',
+            ended_at: null,
+            description:"do some stuff..."
+        },
+        initialize: function() {
+            _.bindAll(this,'start_now','finish_now')
+            this.start_now();
+        },
+        start_now: function() {
+            var now = new Date();
+            this.save({started_at:now.getTime()})
+        },
+        finish_now: function() {
+            var now = new Date();
+            this.save({ended_at:now.getTime(),finished:true})
+        }
     });
+    
+    var WorkChunkList = Backbone.Collection.extend({
+        model: WorkChunk,
+        localStorage: new Store("work_chunks"),
+        active: function(){
+            return this.detect(function(chunk){ return chunk.ended_at })
+        },
+        
+        comparator: function(chunk) {
+            return chunk.get('started_at');
+        }
+    });
+    var WorkChunks = new WorkChunkList;
+    
     
     var WorkChunkView = Backbone.View.extend({
-       model: WorkChunk,
-       
-       el: 'li',
-       
+        tagName: "li",
+        class_name: 'work_chunk',
+        
+        events: {
+            'click .finish':'handleFinished'
+        },
+        
+        template: _.template($('#work_chunk_template').html()),
+        
+        initialize: function() {
+            _.bindAll(this, 'render')
+            this.model.bind('change', this.render)
+            // this.model.view = this;
+        },
+        
+        handleFinished: function(e) {
+            e.preventDefault();
+            this.model.finish_now();
+        },
+        
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        }
     });
     
-    var MainView = Backbone.View.extend({
+    var AppView = Backbone.View.extend({
         initialize: function() {
-            _.bindAll(this, 'startWork')
+            _.bindAll(this, 'addWorkChunk','toggleNewWorkForm')
+            
+            WorkChunks.bind('add', this.addWorkChunk)
+            WorkChunks.bind('add', this.toggleNewWorkForm)
+            WorkChunks.bind('change', this.toggleNewWorkForm)
+            
+            this.resetForm();
         },
     
         el: $('body'),
     
         events: {
-           'submit #new_work_chunk':'startWork'
+           'submit #new_work_chunk':'addOnFormSubmit'
         },
     
-        startWork: function(e) {
+        addOnFormSubmit: function(e) {
             e.preventDefault();
-            $('form#new_work_chunk').hide().find('input[type=text]').val("");
+            
+            var description = $('#new_work_chunk input[type=text]').val();
+            WorkChunks.create({description:description})            
+        },
+        
+        addWorkChunk: function(work_chunk) {
+            var view = new WorkChunkView({model: work_chunk})
+            this.$("#work_chunks").prepend(view.render().el)
+        },
+        
+        hasActiveWork: function() {
+            return WorkChunks.any(function(chunk){ return !chunk.get('ended_at') })
+        },
+        toggleNewWorkForm: function() {
+          if(this.hasActiveWork()) {
+              $('form#new_work_chunk').hide().find('input[type=text]').val('').blur();
+          } else {
+              this.resetForm();
+          }
+        },
+        resetForm: function() {
+            $('form#new_work_chunk').show().find('input[type=text]').val('').focus();
         }
     });
-
-    main_view = new MainView;
-    
+    app_view = new AppView;
 });
