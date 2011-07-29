0428a11396334b97fc1614290d90dd6379d081fc
Chris Ricca <christopher.ricca@gmail.com>
2011-07-29 15:07:28 -0400

    addeding appcache content type

--- app.rb
+++ app.rb
@@ -9,7 +9,12 @@ end
 
 get '/app.appcache' do
   # cache a bit
+  content_type :appcache
   headers['Cache-Control'] = "public, max-age=1000"
   @gitstamp = `git show-ref | grep refs/heads/master`.gsub('refs/heads/master','')
   erb :appcache
+end
+
+configure do
+  mime_type :appcache, 'text/cache-manifest'
 end
