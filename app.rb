e4df170d5743d05d3c482440836a1933d999caea
Chris Ricca <christopher.ricca@gmail.com>
2011-07-29 15:02:04 -0400

    added app cache for offline goodness

--- app.rb
+++ app.rb
@@ -6,3 +6,10 @@ require 'erb'
 get '/' do
   erb :app
 end
+
+get '/app.appcache' do
+  # cache a bit
+  headers['Cache-Control'] = "public, max-age=1000"
+  @gitstamp = `git show-ref | grep refs/heads/master`.gsub('refs/heads/master','')
+  erb :appcache
+end
