5d07aef92cc926d524ba5a9422c841665dd9f6da
Chris Ricca <christopher.ricca@gmail.com>
2011-07-30 14:16:12 -0400

    darker styling

--- app.rb
+++ app.rb
@@ -7,11 +7,16 @@ get '/' do
   erb :app
 end
 
-
-
 get '/app.appcache' do
   content_type 'text/cache-manifest'
-  headers['Cache-Control'] = "public, max-age=1"
-  @gitstamp = ENV['COMMIT_HASH'] || rand(1000000000).to_s(16) # never cache in development
+  # headers['Cache-Control'] = "public, max-age=1"
   erb :appcache
+end
+
+
+
+helpers do
+  def version
+    @version ||= ENV['COMMIT_HASH'] || rand(1000000000).to_s(16) # never cache in development
+  end
 end
